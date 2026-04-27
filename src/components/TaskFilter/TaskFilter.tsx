b17f13c3abe835c486bf1833d0d812dacc447368
Albina93 <alba.tleubergen@gmail.com>
2026-04-27 15:27:35 -0400

    Added priority filter...

--- src/components/TaskFilter/TaskFilter.tsx
+++ src/components/TaskFilter/TaskFilter.tsx
@@ -4,6 +4,7 @@ import type { TaskFilterProps } from "../../types";
 export const Taskfilter: React.FC<TaskFilterProps> = ({ onFilterChange }) => {
   return (
     <div style={{ marginTop: "20px", marginBottom: "30px" }}>
+      {/* Status filter  */}
       <label>
         Status{" "}
         <select
@@ -14,10 +15,28 @@ export const Taskfilter: React.FC<TaskFilterProps> = ({ onFilterChange }) => {
             })
           }
         >
-          <option value="all">All statuses</option>
+          <option value="all">All Statuses</option>
           <option value="pending">Pending</option>
           <option value="in-progress">In Progress</option>
           <option value="completed">Completed</option>
+        </select>
+      </label>
+
+      {/* Priority filter */}
+      <label style={{ marginLeft: "20px" }}>
+        Priority{" "}
+        <select
+          onChange={(e) =>
+            onFilterChange({
+              priority:
+                e.target.value === "all" ? undefined : (e.target.value as any),
+            })
+          }
+        >
+          <option value="all">All priorities</option>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
         </select>
       </label>
     </div>
